34aad01711526e3f193040ea1a4e36cd252f0bbf
Ricardo Vega <vegajuarezricardo88880@gmail.com>
2018-02-09 10:13:38 -0600

    Creacion del proyecto

---
+++ WebPrueba/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace WebPrueba.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        public IActionResult Ruta()
+        {
+            return View();
+        }
+        public IActionResult Notificacion()
+        {
+            return View();
+        }
+    }
+}
